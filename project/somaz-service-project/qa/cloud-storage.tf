2b47e6a53a87819f6b2dd3da1968d9f1ee8afbf8
somaz <genius5711@gmail.com>
2023-10-31 17:30:11 +0900

    style: style: project & modules

--- project/somaz-service-project/qa/cloud-storage.tf
+++ project/somaz-service-project/qa/cloud-storage.tf
@@ -1,11 +1,11 @@
 ## GCS Buckets(Cloud Storage=S3) ##
 module "gcs_buckets" {
-  source     = "../../../modules/gcs_buckets"
-  names      = keys(local.buckets_versioning)
-  project_id = var.project
-  location   = var.region
-  labels     = local.default_labels
-  versioning = local.buckets_versioning
+  source          = "../../../modules/gcs_buckets"
+  names           = keys(local.buckets_versioning)
+  project_id      = var.project
+  location        = var.region
+  labels          = local.default_labels
+  versioning      = local.buckets_versioning
   lifecycle_rules = local.lifecycle_rules
 
 }
